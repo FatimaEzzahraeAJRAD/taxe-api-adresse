7ff3b01405bdb2171ba526ea46167a0212f95db7
user <user@user-PC>
2019-02-12 15:40:38 +0100

    version4

--- src/main/java/com/sir/adresseapi/bean/Redevable.java
+++ src/main/java/com/sir/adresseapi/bean/Redevable.java
@@ -27,6 +27,9 @@ public class Redevable implements Serializable {
     private Long id;
     private String cin;
     private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
     @OneToMany(mappedBy = "redevable")
     private List<Local> locals;
 
@@ -52,6 +55,30 @@ public class Redevable implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
     }
 
     public List<Local> getLocals() {
